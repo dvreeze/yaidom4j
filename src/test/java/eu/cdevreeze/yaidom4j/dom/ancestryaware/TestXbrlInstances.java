f3b0394c8ecfe82422ee6c626cc6a01c4d92a212
Chris de Vreeze <chris.de.vreeze@caiway.net>
2024-08-09 21:14:53 +0200

    More test code

---
+++ src/test/java/eu/cdevreeze/yaidom4j/dom/ancestryaware/TestXbrlInstances.java
@@ -0,0 +1,421 @@
+/*
+ * Copyright 2024-2024 Chris de Vreeze
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package eu.cdevreeze.yaidom4j.dom.ancestryaware;
+
+import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+
+import javax.xml.namespace.QName;
+import java.time.LocalDate;
+import java.util.Optional;
+import java.util.Set;
+import java.util.function.Function;
+
+import static eu.cdevreeze.yaidom4j.dom.ancestryaware.ElementPredicates.hasName;
+
+/**
+ * XBRL instance data model. Incorrect and incomplete, but good enough for the tests it is used in.
+ *
+ * @author Chris de Vreeze
+ */
+public class TestXbrlInstances {
+
+    private static final String XBRLI_NS = "http://www.xbrl.org/2003/instance";
+    private static final String XBRLDI_NS = "http://xbrl.org/2006/xbrldi";
+    private static final String LINK_NS = "http://www.xbrl.org/2003/linkbase";
+    private static final String XLINK_NS = "http://www.w3.org/1999/xlink";
+
+    private TestXbrlInstances() {
+    }
+
+    public interface XbrlInstancePart {
+
+        ElementTree.Element element();
+    }
+
+    public static final class Identifier implements XbrlInstancePart {
+
+        private final ElementTree.Element element;
+
+        public Identifier(ElementTree.Element element) {
+            Preconditions.checkArgument(element.name().equals(new QName(XBRLI_NS, "identifier")));
+            Preconditions.checkArgument(
+                    element.parentElementOption().stream().anyMatch(hasName(XBRLI_NS, "entity"))
+            );
+            this.element = element;
+        }
+
+        @Override
+        public ElementTree.Element element() {
+            return element;
+        }
+
+        public String scheme() {
+            return element.attribute(new QName("scheme"));
+        }
+
+        public String identifierValue() {
+            return element.text().strip();
+        }
+    }
+
+    public static final class ExplicitMember implements XbrlInstancePart {
+
+        private final ElementTree.Element element;
+
+        public ExplicitMember(ElementTree.Element element) {
+            Preconditions.checkArgument(element.name().equals(new QName(XBRLDI_NS, "explicitMember")));
+            Preconditions.checkArgument(
+                    element.ancestorElementStream().anyMatch(hasName(XBRLI_NS, "context"))
+            );
+            this.element = element;
+        }
+
+        @Override
+        public ElementTree.Element element() {
+            return element;
+        }
+
+        public QName dimension() {
+            String dimensionAsString = element.attribute(new QName("dimension"));
+            return element.underlyingElement().namespaceScope().resolveSyntacticQName(dimensionAsString);
+        }
+
+        public QName member() {
+            String memberAsString = element.text().strip();
+            return element.underlyingElement().namespaceScope().resolveSyntacticQName(memberAsString);
+        }
+    }
+
+    public static final class Segment implements XbrlInstancePart {
+
+        private final ElementTree.Element element;
+
+        public Segment(ElementTree.Element element) {
+            Preconditions.checkArgument(element.name().equals(new QName(XBRLI_NS, "segment")));
+            Preconditions.checkArgument(
+                    element.ancestorElementStream().anyMatch(hasName(XBRLI_NS, "context"))
+            );
+            this.element = element;
+        }
+
+        @Override
+        public ElementTree.Element element() {
+            return element;
+        }
+
+        public ImmutableList<ExplicitMember> explicitMembers() {
+            return element.childElementStream(hasName(XBRLDI_NS, "explicitMember"))
+                    .map(ExplicitMember::new)
+                    .collect(ImmutableList.toImmutableList());
+        }
+    }
+
+    public static final class Entity implements XbrlInstancePart {
+
+        private final ElementTree.Element element;
+
+        public Entity(ElementTree.Element element) {
+            Preconditions.checkArgument(element.name().equals(new QName(XBRLI_NS, "entity")));
+            Preconditions.checkArgument(
+                    element.ancestorElementStream().anyMatch(hasName(XBRLI_NS, "context"))
+            );
+            this.element = element;
+        }
+
+        @Override
+        public ElementTree.Element element() {
+            return element;
+        }
+
+        public Identifier identifier() {
+            return element.childElementStream(hasName(XBRLI_NS, "identifier"))
+                    .map(Identifier::new)
+                    .findFirst()
+                    .orElseThrow();
+        }
+
+        public Optional<Segment> segmentOption() {
+            return element.childElementStream(hasName(XBRLI_NS, "segment"))
+                    .map(Segment::new)
+                    .findFirst();
+        }
+    }
+
+    public interface Period extends XbrlInstancePart {
+
+        static Period fromElement(ElementTree.Element element) {
+            Preconditions.checkArgument(element.name().equals(new QName(XBRLI_NS, "period")));
+            Preconditions.checkArgument(
+                    element.ancestorElementStream().anyMatch(hasName(XBRLI_NS, "context"))
+            );
+
+            if (element.childElementStream(hasName(XBRLI_NS, "instant")).findAny().isPresent()) {
+                return new InstantPeriod(element);
+            } else {
+                return new StartEndDatePeriod(element);
+            }
+        }
+    }
+
+    public static final class InstantPeriod implements Period {
+
+        private final ElementTree.Element element;
+
+        public InstantPeriod(ElementTree.Element element) {
+            Preconditions.checkArgument(element.name().equals(new QName(XBRLI_NS, "period")));
+            Preconditions.checkArgument(element.childElementStream(hasName(XBRLI_NS, "instant")).findAny().isPresent());
+            this.element = element;
+        }
+
+        @Override
+        public ElementTree.Element element() {
+            return element;
+        }
+
+        public LocalDate instant() {
+            return LocalDate.parse(
+                    element.childElementStream(hasName(XBRLI_NS, "instant"))
+                            .findFirst()
+                            .orElseThrow()
+                            .text()
+            );
+        }
+    }
+
+    public static final class StartEndDatePeriod implements Period {
+
+        private final ElementTree.Element element;
+
+        public StartEndDatePeriod(ElementTree.Element element) {
+            Preconditions.checkArgument(element.name().equals(new QName(XBRLI_NS, "period")));
+            Preconditions.checkArgument(element.childElementStream(hasName(XBRLI_NS, "startDate")).findAny().isPresent());
+            Preconditions.checkArgument(element.childElementStream(hasName(XBRLI_NS, "endDate")).findAny().isPresent());
+            this.element = element;
+        }
+
+        @Override
+        public ElementTree.Element element() {
+            return element;
+        }
+
+        public LocalDate startDate() {
+            return LocalDate.parse(
+                    element.childElementStream(hasName(XBRLI_NS, "startDate"))
+                            .findFirst()
+                            .orElseThrow()
+                            .text()
+            );
+        }
+
+        public LocalDate endDate() {
+            return LocalDate.parse(
+                    element.childElementStream(hasName(XBRLI_NS, "endDate"))
+                            .findFirst()
+                            .orElseThrow()
+                            .text()
+            );
+        }
+    }
+
+    public static final class Context implements XbrlInstancePart {
+
+        private final ElementTree.Element element;
+
+        public Context(ElementTree.Element element) {
+            Preconditions.checkArgument(element.name().equals(new QName(XBRLI_NS, "context")));
+            this.element = element;
+        }
+
+        @Override
+        public ElementTree.Element element() {
+            return element;
+        }
+
+        public String id() {
+            return element.attribute(new QName("id"));
+        }
+
+        public Entity entity() {
+            return element.childElementStream(hasName(XBRLI_NS, "entity"))
+                    .map(Entity::new)
+                    .findAny()
+                    .orElseThrow();
+        }
+
+        public Period period() {
+            return element.childElementStream(hasName(XBRLI_NS, "period"))
+                    .map(Period::fromElement)
+                    .findAny()
+                    .orElseThrow();
+        }
+    }
+
+    public static final class Measure implements XbrlInstancePart {
+
+        private final ElementTree.Element element;
+
+        public Measure(ElementTree.Element element) {
+            Preconditions.checkArgument(element.name().equals(new QName(XBRLI_NS, "measure")));
+            Preconditions.checkArgument(
+                    element.ancestorElementStream().anyMatch(hasName(XBRLI_NS, "unit"))
+            );
+            this.element = element;
+        }
+
+        @Override
+        public ElementTree.Element element() {
+            return element;
+        }
+
+        public QName measure() {
+            String measureAsString = element.text();
+            return element.underlyingElement().namespaceScope().resolveSyntacticQName(measureAsString);
+        }
+    }
+
+    public static final class Unit implements XbrlInstancePart {
+
+        private final ElementTree.Element element;
+
+        public Unit(ElementTree.Element element) {
+            Preconditions.checkArgument(element.name().equals(new QName(XBRLI_NS, "unit")));
+            this.element = element;
+        }
+
+        @Override
+        public ElementTree.Element element() {
+            return element;
+        }
+
+        public String id() {
+            return element.attribute(new QName("id"));
+        }
+
+        public ImmutableList<Measure> measures() {
+            return element.childElementStream(hasName(XBRLI_NS, "measure"))
+                    .map(Measure::new)
+                    .collect(ImmutableList.toImmutableList());
+        }
+    }
+
+    public interface Fact extends XbrlInstancePart {
+    }
+
+    public static final class ItemFact implements Fact {
+
+        private final ElementTree.Element element;
+
+        public ItemFact(ElementTree.Element element) {
+            Preconditions.checkArgument(!Set.of(XBRLI_NS, LINK_NS).contains(element.name().getNamespaceURI()));
+            Preconditions.checkArgument(
+                    element.ancestorElementStream()
+                            .filter(e -> !hasName(XBRLI_NS, "xbrl").test(e))
+                            .noneMatch(
+                                    e -> e.name().getNamespaceURI().equals(XBRLI_NS) ||
+                                            e.name().getNamespaceURI().equals(LINK_NS)
+                            )
+            );
+            this.element = element;
+        }
+
+        @Override
+        public ElementTree.Element element() {
+            return element;
+        }
+
+        public QName factName() {
+            return element.name();
+        }
+
+        public String contextRef() {
+            return element.attribute(new QName("contextRef"));
+        }
+
+        public Optional<String> unitRefOption() {
+            return element.attributeOption(new QName("unitRef"));
+        }
+
+        public Optional<String> decimalsOption() {
+            return element.attributeOption(new QName("decimals"));
+        }
+
+        public String rawFactValue() {
+            return element.text();
+        }
+    }
+
+    public static final class XbrlInstance implements XbrlInstancePart {
+
+        private final ElementTree.Element element;
+        private final ImmutableMap<String, Context> contexts;
+        private final ImmutableMap<String, Unit> units;
+
+        private XbrlInstance(
+                ElementTree.Element element,
+                ImmutableMap<String, Context> contexts,
+                ImmutableMap<String, Unit> units
+        ) {
+            Preconditions.checkArgument(element.name().equals(new QName(XBRLI_NS, "xbrl")));
+            this.element = element;
+            this.contexts = contexts;
+            this.units = units;
+        }
+
+        @Override
+        public ElementTree.Element element() {
+            return element;
+        }
+
+        public ImmutableMap<String, Context> contexts() {
+            return contexts;
+        }
+
+        public ImmutableMap<String, Unit> units() {
+            return units;
+        }
+
+        public ImmutableList<ItemFact> topLevelItemFacts() {
+            return element
+                    .childElementStream(hasName(nm -> !Set.of(XBRLI_NS, LINK_NS).contains(nm.getNamespaceURI())))
+                    .map(ItemFact::new)
+                    .collect(ImmutableList.toImmutableList());
+        }
+
+        public ImmutableList<ItemFact> topLevelItemFacts(QName factName) {
+            return topLevelItemFacts()
+                    .stream()
+                    .filter(f -> f.factName().equals(factName))
+                    .collect(ImmutableList.toImmutableList());
+        }
+
+        public static XbrlInstance from(ElementTree.Element element) {
+            ImmutableMap<String, Context> contexts =
+                    element.childElementStream(hasName(XBRLI_NS, "context"))
+                            .map(Context::new)
+                            .collect(ImmutableMap.toImmutableMap(Context::id, Function.identity()));
+
+            ImmutableMap<String, Unit> units =
+                    element.childElementStream(hasName(XBRLI_NS, "unit"))
+                            .map(Unit::new)
+                            .collect(ImmutableMap.toImmutableMap(Unit::id, Function.identity()));
+
+            return new XbrlInstance(element, contexts, units);
+        }
+    }
+}
---
+++ src/test/java/eu/cdevreeze/yaidom4j/dom/ancestryaware/XbrlInstanceQueryTests.java
@@ -0,0 +1,185 @@
+/*
+ * Copyright 2024-2024 Chris de Vreeze
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package eu.cdevreeze.yaidom4j.dom.ancestryaware;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import eu.cdevreeze.yaidom4j.dom.immutabledom.Documents;
+import eu.cdevreeze.yaidom4j.dom.immutabledom.jaxpinterop.ImmutableDomProducingSaxHandler;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import javax.xml.namespace.QName;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+/**
+ * XBRL instance query tests.
+ * <p>
+ * This is not a regular unit test, in that it assumes parsing etc. to work correctly.
+ *
+ * @author Chris de Vreeze
+ */
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class XbrlInstanceQueryTests {
+
+    private static final String XBRLI_NS = "http://www.xbrl.org/2003/instance";
+    private static final String XBRLDI_NS = "http://xbrl.org/2006/xbrldi";
+    private static final String LINK_NS = "http://www.xbrl.org/2003/linkbase";
+    private static final String XLINK_NS = "http://www.w3.org/1999/xlink";
+
+    private static final String ISO4217_NS = "http://www.xbrl.org/2003/iso4217";
+
+    private static final String GAAP_NS = "http://xasb.org/gaap";
+
+    private TestXbrlInstances.XbrlInstance instance;
+
+    @BeforeAll
+    void parseDocument() throws SAXException, ParserConfigurationException, IOException {
+        SAXParserFactory saxParserFactory = SAXParserFactory.newDefaultInstance();
+        saxParserFactory.setNamespaceAware(true); // Important!
+        saxParserFactory.setValidating(false);
+        saxParserFactory.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+        SAXParser parser = saxParserFactory.newSAXParser();
+        ImmutableDomProducingSaxHandler saxHandler = new ImmutableDomProducingSaxHandler();
+
+        InputStream inputStream = BookQueryTests.class.getResourceAsStream("/sample-xbrl-instance.xml");
+        parser.parse(new InputSource(inputStream), saxHandler);
+        var underlyingDoc = Documents.removeInterElementWhitespace(saxHandler.resultingDocument());
+        instance = TestXbrlInstances.XbrlInstance.from(Document.from(underlyingDoc).documentElement());
+    }
+
+    @Test
+    void testQueryDimensionMembers() {
+        TestXbrlInstances.Context context = Objects.requireNonNull(instance.contexts().get("I-2005"));
+        ImmutableMap<QName, QName> dimensionMembers =
+                context.entity().segmentOption().orElseThrow()
+                        .explicitMembers()
+                        .stream()
+                        .map(e -> Map.entry(e.dimension(), e.member()))
+                        .collect(ImmutableMap.toImmutableMap(Map.Entry::getKey, Map.Entry::getValue));
+
+        ImmutableMap<QName, QName> expectedDimensionMembers = ImmutableMap.of(
+                new QName(GAAP_NS, "EntityAxis"), new QName(GAAP_NS, "ABCCompanyDomain"),
+                new QName(GAAP_NS, "BusinessSegmentAxis"), new QName(GAAP_NS, "ConsolidatedGroupDomain"),
+                new QName(GAAP_NS, "VerificationAxis"), new QName(GAAP_NS, "UnqualifiedOpinionMember"),
+                new QName(GAAP_NS, "PremiseAxis"), new QName(GAAP_NS, "ActualMember"),
+                new QName(GAAP_NS, "ReportDateAxis"), new QName(GAAP_NS, "ReportedAsOfMarch182008Member")
+        );
+
+        assertEquals(expectedDimensionMembers, dimensionMembers);
+    }
+
+    @Test
+    void testQueryItemFactContext() {
+        ImmutableList<TestXbrlInstances.ItemFact> landFacts = instance.topLevelItemFacts(new QName(GAAP_NS, "Land"));
+
+        assertEquals(3, landFacts.size());
+
+        ImmutableList<TestXbrlInstances.ItemFact> landFacts2005 = landFacts
+                .stream()
+                .filter(f -> f.contextRef().equals("I-2005"))
+                .collect(ImmutableList.toImmutableList());
+
+        assertEquals(1, landFacts2005.size());
+
+        TestXbrlInstances.ItemFact landFact2005 = landFacts2005.get(0);
+
+        TestXbrlInstances.Context context = instance.contexts().get(landFact2005.contextRef());
+
+        assertNotNull(context);
+
+        ImmutableMap<QName, QName> dimensionMembers =
+                context.entity().segmentOption().orElseThrow()
+                        .explicitMembers()
+                        .stream()
+                        .map(e -> Map.entry(e.dimension(), e.member()))
+                        .collect(ImmutableMap.toImmutableMap(Map.Entry::getKey, Map.Entry::getValue));
+
+        ImmutableMap<QName, QName> expectedDimensionMembers = ImmutableMap.of(
+                new QName(GAAP_NS, "EntityAxis"), new QName(GAAP_NS, "ABCCompanyDomain"),
+                new QName(GAAP_NS, "BusinessSegmentAxis"), new QName(GAAP_NS, "ConsolidatedGroupDomain"),
+                new QName(GAAP_NS, "VerificationAxis"), new QName(GAAP_NS, "UnqualifiedOpinionMember"),
+                new QName(GAAP_NS, "PremiseAxis"), new QName(GAAP_NS, "ActualMember"),
+                new QName(GAAP_NS, "ReportDateAxis"), new QName(GAAP_NS, "ReportedAsOfMarch182008Member")
+        );
+
+        assertEquals(expectedDimensionMembers, dimensionMembers);
+    }
+
+    @Test
+    void testQueryItemFactUnit() {
+        ImmutableList<TestXbrlInstances.ItemFact> landFacts = instance.topLevelItemFacts(new QName(GAAP_NS, "Land"));
+
+        assertEquals(3, landFacts.size());
+
+        ImmutableList<TestXbrlInstances.ItemFact> landFacts2007 = landFacts
+                .stream()
+                .filter(f -> f.contextRef().equals("I-2007"))
+                .collect(ImmutableList.toImmutableList());
+
+        assertEquals(1, landFacts2007.size());
+
+        TestXbrlInstances.ItemFact landFact2007 = landFacts2007.get(0);
+
+        TestXbrlInstances.Unit unit = instance.units().get(landFact2007.unitRefOption().orElse(""));
+
+        assertNotNull(unit);
+
+        String unitRef = landFact2007.unitRefOption().orElse("");
+
+        List<QName> measures = Objects.requireNonNull(instance.units().get(unitRef))
+                .measures()
+                .stream()
+                .map(TestXbrlInstances.Measure::measure)
+                .toList();
+
+        ImmutableList<QName> expectedMeasures = ImmutableList.of(new QName(ISO4217_NS, "USD"));
+
+        assertEquals(expectedMeasures, measures);
+    }
+
+    @Test
+    void testQueryItemFactValue() {
+        ImmutableList<TestXbrlInstances.ItemFact> preferredStockAmountFacts = instance.topLevelItemFacts(new QName(GAAP_NS, "PreferredStockAmount"));
+
+        assertEquals(6, preferredStockAmountFacts.size());
+
+        ImmutableList<TestXbrlInstances.ItemFact> preferredStockAmountFacts2007PsAll = preferredStockAmountFacts
+                .stream()
+                .filter(f -> f.contextRef().equals("I-2007-PS-All"))
+                .collect(ImmutableList.toImmutableList());
+
+        assertEquals(1, preferredStockAmountFacts2007PsAll.size());
+
+        TestXbrlInstances.ItemFact preferredStockAmountFact2007PsAll = preferredStockAmountFacts2007PsAll.get(0);
+
+        assertEquals(String.valueOf(2000), preferredStockAmountFact2007PsAll.rawFactValue().strip());
+    }
+}
