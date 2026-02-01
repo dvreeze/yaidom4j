443dcf6ba476ff339a3e00aabb64048d60e8080b
Chris de Vreeze <chris.de.vreeze@caiway.net>
2026-02-01 19:20:17 +0100

    Turned into Java Module

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module eu.cdevreeze.yaidom4j {
+    requires transitive com.google.common;
+    requires transitive java.xml;
+    requires java.logging; // Only for unit testing...
+
+    exports eu.cdevreeze.yaidom4j.core;
+    exports eu.cdevreeze.yaidom4j.dom.ancestryaware;
+    exports eu.cdevreeze.yaidom4j.dom.clark;
+    exports eu.cdevreeze.yaidom4j.dom.clark.jaxpinterop;
+    exports eu.cdevreeze.yaidom4j.dom.immutabledom;
+    exports eu.cdevreeze.yaidom4j.dom.immutabledom.jaxpinterop;
+    exports eu.cdevreeze.yaidom4j.jaxp;
+    exports eu.cdevreeze.yaidom4j.queryapi;
+    exports eu.cdevreeze.yaidom4j.transformationapi;
+}
